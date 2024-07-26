3a4cc7337a7244139342cee67e249d4510c57a1d
Marc Guinea <develop.marcguinea@gmail.com>
2024-07-26 04:19:24 +0200

    Actualizar laravel-pages.php

--- config/laravel-pages.php
+++ config/laravel-pages.php
@@ -2,30 +2,16 @@
 
 return [
     'models' => [
-        'entry' => \Mguinea\Pages\Models\Entry::class,
         'locale' => \Mguinea\Pages\Models\Locale::class,
         'page' => \Mguinea\Pages\Models\Page::class,
-        'route' => \Mguinea\Pages\Models\Route::class,
         'user' => \App\Models\User::class,
-        'view' => \Mguinea\Pages\Models\View::class,
     ],
 
     'table_names' => [
-        'entries' => 'lp_entries',
-        'locales' => 'lp_locales',
-        'pages' => 'lp_pages',
-        'routes' => 'lp_routes',
+        'locales' => 'lps_locales',
+        'pages' => 'lps_pages',
         'users' => 'users',
-        'views' => 'lp_views',
     ],
 
-    'route_loader' => \Mguinea\Pages\RouteLoader::class,
-
     'route_loader_enabled' => true,
-
-    'base_url' => env('APP_URL', 'http://localhost:8000/'),
-
-    'controllers' => [
-        'show_page' => \Mguinea\Pages\Http\Controllers\ShowPageController::class
-    ]
 ];
